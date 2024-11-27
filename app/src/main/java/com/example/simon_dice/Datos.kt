753e81224b821577ceaeeb2712d61ac7804f95f0
DavidLealAra <dlealaraujo@danielcastelao.org>
2024-11-27 19:04:35 +0100

    Creo la clase Datos y le añado dos variables

--- app/src/main/java/com/example/simon_dice/Datos.kt
+++ app/src/main/java/com/example/simon_dice/Datos.kt
@@ -1,2 +1,14 @@
 package com.example.simon_dice
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * Clase Datos que se encarga de almacenar los datos de la aplicación.
+ */
+object Datos {
+    // Se inicializa la variable numero en 0
+    var numero: Int = 0
+    var ronda : MutableLiveData <Int> = MutableLiveData(0)
+}
