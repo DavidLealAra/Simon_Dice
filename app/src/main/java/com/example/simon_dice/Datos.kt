e9ef5531d95205eff9cc9a23b86c26281b36c527
DavidLealAra <dlealaraujo@danielcastelao.org>
2024-11-27 19:09:03 +0100

    Creo una enum class para los colores.

--- app/src/main/java/com/example/simon_dice/Datos.kt
+++ app/src/main/java/com/example/simon_dice/Datos.kt
@@ -12,3 +12,13 @@ object Datos {
     var numero: Int = 0
     var ronda : MutableLiveData <Int> = MutableLiveData(0)
 }
+
+/**
+ * Enumeración de los colores de los botones.
+ */
+enum class ColorButton(val color: Color, val label: String, val value: Int) {
+    VERDE(Color.Green, "Verde", 1),
+    ROJO(Color.Red, "Rojo", 2),
+    AMARILLO(Color.Yellow, "Amarillo", 3),
+    AZUL(Color.Blue, "Azul", 4)
+}
